c9c0ea1a2561dbc800541f54427ba2e8549d8214
joelthiek <joelrokiemlothiek@gmail.com>
2024-10-03 20:48:59 +0530

    fix landing page

---
+++ landingPage/landingPage.js
@@ -0,0 +1,5 @@
+const start_drawing_btn = document.getElementById("start_drawing")
+
+start_drawing_btn.addEventListener("click", function(){
+    window.location.href = "/whiteboard/index.html"
+})
